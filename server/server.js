8559a5136b18cf437f1a54f78748e404b88b4384
merepapa <82329144+merepapa@users.noreply.github.com>
2024-09-24 19:25:19 +0530

    Updating committed

--- server/server.js
+++ server/server.js
@@ -10,11 +10,9 @@ import cors from "cors";
 const app = express();
 const upload = multer({ dest: "uploads/" });
 app.use(cors());
-// IPFS client setup
 const client = create();
 
 const d = new Date();
-// let time = d.getTime();
 console.log(d.toLocaleString());
 
 fs.createReadStream("output.csv")
@@ -27,42 +25,34 @@ fs.createReadStream("output.csv")
   });
 
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "public")));
 
-// Function to add a new line to a string
 const addNewLine = (s) => s + "\n";
 
-// Function to append CID to a CSV file
 const appendToCSV = (cid) => {
   let temp = `\n1,${cid},${d.toLocaleString()}`;
   fs.appendFileSync("output.csv", temp, addNewLine);
 };
 
-// Route to handle form submission
 app.post("/submit", upload.single("file"), async (req, res) => {
   try {
     let content;
 
-    // Check if text is present
     if (req.body.text) {
       content = req.body.text;
     }
 
-    // Check if a file is uploaded
     if (req.file) {
       content = fs.readFileSync(req.file.path);
     }
 
     if (content) {
-      // Upload content to IPFS
       console.log(content);
       const { cid } = await client.add({ content });
       console.log("Content CID:", cid.toString());
 
-      // Save the CID to a local CSV file
       appendToCSV(cid.toString());
     }
-    //changed data
+
     res.send("Data received and CID saved at = ");
   } catch (error) {
     console.error("Error processing request:", error);
